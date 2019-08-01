d506e070dd076c276abd1007117c328d71e39427
Vrana, Jozef <jozef.vrana@wirecard.com>
2019-08-01 10:12:08 +0200

    project init

---
+++ Forecaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecaster
+//
+//  Created by Vrana, Jozef on 01/08/2019.
+//  Copyright © 2019 Vrana, Jozef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
